19d32f38a666c8368addbf03b2839cfff29688f0
Victor Locoman <victor@locoman.ro>
2019-05-06 22:12:59 +0300

    Added state to home component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,21 @@
-import Link from 'next/link';
+import React from 'react'
+import Link from 'next/link'
 
-function Home() {
-	return <div>
-		<h1>Welcome to Next.js!</h1>
-		<p>This was a quick static site demo!</p>
-		<p>Go to <Link href="/about"><a>About</a></Link></p>
-	</div>
+class Home extends React.Component {
+	state = {
+		name: 'Victor'
+	}
+	render() {
+		const { name } = this.state
+		return (
+			<div>
+				<h1>Welcome to Next.js!</h1>
+				<p>This was a quick static site demo!</p>
+				<p>I'm {name}</p>
+				<p>Go to <Link href="/about"><a>About</a></Link></p>
+			</div>
+		)
+	}
 }
 
 export default Home
