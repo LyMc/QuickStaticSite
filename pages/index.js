c29974cb0df195a4e02ff3457bf449e43e3465a6
Victor Locoman <victor@locoman.ro>
2019-05-06 21:40:14 +0300

    Changed Home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-	return <div>Welcome to Next.js!</div>
+	return <div>
+		<h1>Welcome to Next.js!</h1>
+		<p>This was a quick static site demo!</p>
+	</div>
 }
 
 export default Home
